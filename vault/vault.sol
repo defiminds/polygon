3420a05a6ca9a981155890f2ddab1751de4824d4
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-28 15:35:37 +0000

    Support to DAI Token

--- vault/vault.sol
+++ vault/vault.sol
@@ -13,6 +13,7 @@ contract DepositWithdraw {
     function deposit(address token, uint256 amount) public {
         require(token == 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270 || // WMATIC
                 token == 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619 || // WETH
+                token == 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063 || // DAI
                 token == 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6,  // WBTC
                 "Token not supported"); // Ensure token is supported
         require(amount > 0, "Amount must be greater than zero");
